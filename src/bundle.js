013f4deda314fc10816d6dad799c14350e75d1ec
Dave Judd <52185dj@gmail.com>
2023-03-22 13:01:52 -0500

    Add fonts

--- src/bundle.js
+++ src/bundle.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
+const fse = require("fs-extra");
 const pug = require('pug');
 const data = require('./numeronymsData.json');
 
 const compiledFunction = pug.compileFile('./src/index.pug');
-
 const html = compiledFunction({data: data});
 
 if (!fs.existsSync("dist")){
   fs.mkdirSync("dist");
 }
 
-const stream = fs.createWriteStream('./dist/index.html');
+fse.copySync('fonts', 'dist/fonts');
 
+const stream = fs.createWriteStream('./dist/index.html');
 stream.once('open', (fd) => stream.end(html));
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        oldenburg: ['Oldenburg'],
+        otomanopee: ['Otomanopee One']
+      }
+    },
   },
   plugins: []
 }
