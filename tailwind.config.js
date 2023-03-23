26e489cb6c2e04bf7921c1ee50562159abfc0f37
Dave Judd <52185dj@gmail.com>
2023-03-22 21:21:06 -0500

    Add content and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
+      colors: {
+        'nym-black': '#333333',
+        'nym-blue': '#365C6F',
+        'nym-green': '#30643E',
+        'nym-orange': '#EFC8B3'
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
       fontFamily: {
         oldenburg: ['Oldenburg'],
         otomanopee: ['Otomanopee One']
